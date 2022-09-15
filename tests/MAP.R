079e94636ad5dc5b66e521aa1213f80eb0fdf219
sepmein <sepmein@gmail.com>
2022-09-15 17:06:15 +0800

    adding plot functions

--- tests/MAP.R
+++ tests/MAP.R
@@ -1,5 +1,6 @@
 library(snt)
 
+# add year to this function
 set_country(
     root_folder = "/Users/sepmein/dev/working/snt-data",
     country = "BDI"
@@ -7,16 +8,44 @@ set_country(
 
 bdi_shapefile <-
     CountryShapeFile$new()
-
+bdi_shapefile2 <- CountryShapeFile$new(local_destination = paste(
+  "Countries",
+  snt_country,
+  "2020_SNT",
+  "Analysis",
+  "orig",
+  "data",
+  "shapefiles",
+  "BDI_HFs_2013.shp",
+  sep = "/"
+))
 # PfPR
-Pf_prevalence <- PfPrevalence$new()
-Pf_prevalence$download(destfile = "pfPR.zip")
+Pf_prevalence <- PfPrevalence$new(
+  local_destination = "",
+  output_destination = ""
+)
+# Pf_prevalence$download(destfile = "pfPR.zip")
+# added time when generated these data
 Pf_prevalence$load(country_shape_file = bdi_shapefile)$export()
+# TODO
+# for each districts
+# shadows for ci
+Pf_prevalence$plot_line(
+
+)
+# generate a map per year for mean, lci & uci
+Pf_prevalence$plot_map(
+  year = 2017,
+  color = "Grean",
+  categories = 6,
+  labels = TRUE/FALSE
+)
 
 # PfIC
 Pf_incidence <- PfIncidence$new()
 Pf_incidence$download(destfile = "PfIC.zip")
 Pf_incidence$load(country_shape_file = bdi_shapefile)$export()
+
 
 # PfMT
 Pf_mortality <- PfMortality$new()
