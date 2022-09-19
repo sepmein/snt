f2148bcbf52dc3355a9645ae9b1c3c44035c31bf
张春哲 <sepmein@gmail.com>
2022-09-19 22:14:38 +0800

    add update database function

--- R/import.R
+++ R/import.R
@@ -24,7 +24,7 @@ routine_rename <- function(df, country) {
     }
     df <- df %>%
         dplyr::rename_with(
-           find_and_rename
+            find_and_rename
         )
     return(df)
 }
@@ -100,4 +100,25 @@ routine_replace <- function(df, country) {
             find_and_rename
         )
     return(df)
+}
+
+#' @export
+update_database <- function() {
+    import_routine_rename <- readr::read_csv(
+        "inst//extdata//routine//01_rename.csv"
+    )
+    usethis::use_data(import_routine_rename)
+    import_routine_replace <- readr::read_csv(
+        "inst//extdata//routine//02_replace.csv"
+    )
+    usethis::use_data(import_routine_replace)
+    import_routine_replace_hfname <- readr::read_csv(
+        "inst//extdata//routine//03_replace_hfname.csv"
+    )
+    usethis::use_data(import_routine_replace_hfname)
+    import_routine_set_cluster <- readr::read_csv(
+        "inst//extdata//routine//04_cluster.csv"
+    )
+    usethis::use_data(import_routine_set_cluster)
+    devtools::load_all()
 }
--- implementation/bdi/bdi.R
+++ implementation/bdi/bdi.R
@@ -17,21 +17,15 @@ suspects <- smart_read_excel(
   "Countries/BDI/2020_SNT/Analysis/orig/data/routine/monthly/CAS_SUSPECTES_PALU_EN_*yyyy*.xls",
   skip = 2
 )
-a <- suspects %>%
+suspects <- suspects %>%
   # rename using internal rename database
-  mutate(data = map(
-    data,
-    ~ routine_rename(.x, country = "BDI")
-  )) %>%
+  mutate(data = map(data, ~ routine_rename(.x, country = "BDI"))) %>%
   # remove space + year, from "2 2018" to "2"
-  mutate(data = map(
-    data,
-    ~ rename_with(.x, ~ str_remove(.x, " [12][0-9]{3}$"))
-  )) %>%
+  mutate(data = map(data, ~ rename_with(
+    .x, ~ str_remove(.x, " [12][0-9]{3}$")
+  ))) %>%
+  # replace using internal replace database
+  mutate(data = map(data, ~ routine_replace(.x, country = "BDI"))) %>%
   # combine all susceptible data
-  unnest(data) %>%
-  # reshape data
-  pivot_longer(
-    cols = 7:18, names_to = "month",
-    values_to = "suspect"
-  )
+  unnest(data) %>% # reshape data
+    pivot_longer(cols = 7:18, names_to = "month", values_to = "suspect")
