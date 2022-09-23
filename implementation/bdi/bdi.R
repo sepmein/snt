7b1f7d87e3c2aa047be6950b4205db457b8f15f3
张春哲 <sepmein@gmail.com>
2022-09-23 11:31:15 +0800

    improved comments

--- implementation/bdi/bdi.R
+++ implementation/bdi/bdi.R
@@ -8,11 +8,8 @@ library(tmap)
 set_country(root_folder = "/Users/sepmein/dev/working/snt-data",
             country = "BDI")
 
-#######################
-# Import Routine Data #
-#######################
-
-### suspect cases
+# 1. Importation ---------------------------------------------------------------
+## 1.1 Suspect -------------------------------------------------------------
 susp <- smart_read_excel(
   "Countries/BDI/2020_SNT/Analysis/orig/data/routine/monthly/CAS_SUSPECTES_PALU_EN_*yyyy*.xls",
   skip = 2,
@@ -28,7 +25,7 @@ susp <- susp |>
                names_to = "month",
                values_to = "susp")
 
-### tested and confirmed
+## 1.2 tested and confirmed -----------------------------------------------
 testconf <- smart_read_excel(
   "Countries/BDI/2020_SNT/Analysis/orig/data/routine/monthly/TDR_et_GE_*yyyy*.xls"
 )
@@ -40,7 +37,7 @@ testconf <- testconf |>
   separate(index, sep = "_(?!.*_)", into = c("index", "month")) |>
   pivot_wider(names_from = "index", values_from = "testconf")
 
-### mal treat under 5
+## 1.3 Malaria Treatment Under 5 -------------------------------------------
 maltreat_u5 <- smart_read_excel(
   "countries/bdi/2020_snt/analysis/orig/data/routine/monthly/cas_traites_act_de_moins_de_5_ans_en_*yyyy*.xls",
   skip = 2
@@ -51,7 +48,7 @@ maltreat_u5 <- maltreat_u5 |>
                names_to = "month",
                values_to = "maltreat_u5")
 
-### mal treat over 5
+## 1.4 Malaria Treatment Over 5 --------------------------------------------
 maltreat_ov5 <- smart_read_excel(
   "Countries/BDI/2020_SNT/Analysis/orig/data/routine/monthly/CAS_TRAITES_ACT_5_ANS_ET_PLUS_*yyyy*.xls"
 )
@@ -61,13 +58,11 @@ maltreat_ov5 <- maltreat_ov5 |>
                names_to = "month",
                values_to = "maltreat_ov5")
 
-### malaria death
+## 1.5 Malaria Death -------------------------------------------------------
 maldth <- smart_read_excel(
   "Countries/BDI/2020_SNT/Analysis/orig/data/routine/monthly/Decès_lié_au_palu_*yyyy*.xls"
 )
-
-# 2016 data structure is different from others
-# deal with 2016 first
+# 2016 data structure is different from others deal with 2016 first
 maldth$data[[1]] <- maldth$data[[1]] |>
   pivot_longer(cols = 8:103,
                names_to = "index",
@@ -90,12 +85,13 @@ maldth <- maldth |>
   pivot_wider(names_from = "index", values_from = "value") |>
   select(-Centre)
 
-### merge
+### merge all four databases into one
 routine_monthly <- full_join(susp, testconf)
 routine_monthly <- full_join(routine_monthly, maltreat_u5)
 routine_monthly <- full_join(routine_monthly, maltreat_ov5)
 routine_monthly <- full_join(routine_monthly, maldth)
 
+### Continue data cleaning and transformation
 routine_monthly <- routine_monthly |>
   select(-Pays) |>
   # drop if Districts==""
@@ -103,15 +99,22 @@ routine_monthly <- routine_monthly |>
   # but without district info and only reported
   # in the testconf database in 2016 and 2019
   filter(adm2_ds != "") |>
+  # change from "DS gerate" to "gerate"
   mutate(adm2 = str_remove(adm2_ds, "DS\\s")) |>
+  # remove adm2_ds column
   select(-adm2_ds) |>
+  # create a new column named hf by combine adm1, adm2 & hfname
   mutate(hf = paste(adm1, adm2, hfname, sep = "-")) |>
+  # create yearmon column by year and month
   mutate(yearmon = str_c(year, str_pad(month, 2, pad = "0"))) |>
+  # create a new ID column by row number
   rowid_to_column("ID")
 
 routine_monthly <- routine_monthly |>
+  # compute test and malaria treatment
   mutate(test = test_rdt + test_mic) |>
   mutate(maltreat = maltreat_u5 + maltreat_ov5) |>
+  # select and order the columns
   select(
     ID,
     adm1,
@@ -145,25 +148,34 @@ hfs <-
 routine_monthly <- routine_monthly |>
   left_join(hfs)
 
-# Get Confirmed RDT
+
+# 2. Analyze -----------------------------------------------------------------
+## 2.1 Summaries ---------------------------------------------------------------
+### 2.1.1 N/A values --------------------------------------------------------------
+View(routine_monthly |>
+       na_if("") |>
+       map_df( ~ sum(is.na(.))))
+
+### 2.2.2 Summaries ---------------------------------------------------------
+routine_monthly |> select_if(is_numeric) |> skimr::skim()
+
+## 2.2 Confirmed RDTs ------------------------------------------------------
 conf_rdt <- routine_monthly |>
   arrange(yearmon) |>
   left_join(import_routine_set_cluster) |>
   group_by(yearmon, cluster) |>
   summarise(conf_rdt = sum(conf_rdt, na.rm = TRUE))
 
-### plot by cluster
+# plot by cluster
 ggplot(data = conf_rdt, aes(x = yearmon, y = conf_rdt, group = cluster)) +
   geom_line() +
   scale_x_discrete(guide = guide_axis(check.overlap = TRUE)) +
   labs(title = "Confirmed RDT tests by cluster", y = "Confirmed RDT Test") +
-  facet_wrap( ~ cluster, scales = "free")
+  facet_wrap(~ cluster, scales = "free")
 
-##################
-### Outlier detect
-##################
+## 2.3 Outliers ------------------------------------------------------------
 
-### plot
+### Plot --------------------------------------------------------------------
 routine_monthly |>
   select(
     year,
@@ -191,19 +203,20 @@ routine_monthly |>
                               y = value,
                               pairwise.comparisons = FALSE)
 
-### generate outliers list
+### List --------------------------------------------------------------------
 outliers <- find_outiler(routine_monthly)
 outliers_by_hf <- outliers_find_hf(outliers)
 
 # outlier_test <- EnvStats::rosnerTest(routine_monthly$maldth, 10000)
 # outlier_test$all.stats
 
-### plotting hf reporting status
+## 2.4 Health Facilities Reporting Status ----------------------------------
 report_status_by_hf_and_date <- routine_monthly |>
   group_by(hf, yearmon) |>
   summarise(sum = sum(c_across(susp:maldth), na.rm = TRUE)) |>
   mutate(reported = if_else(sum > 0, "Y", "N"))
 
+### Plot by HF & Date -------------------------------------------------------
 report_status_by_hf_and_date |>
   ggplot(aes(x = yearmon, y = hf, fill = reported)) +
   geom_tile() +
@@ -214,7 +227,7 @@ report_status_by_hf_and_date |>
   theme(axis.text.y = element_blank()) +
   scale_fill_manual(values = c("N" = "white", "Y" = "blue"))
 
-### Calculate report rate
+### Report Rate Table -------------------------------------------------------
 report_rate_by_hf <- report_status_by_hf_and_date |>
   mutate(reported_value = ifelse(reported == "Y", 1, 0)) |>
   group_by(hf) |>
@@ -222,7 +235,7 @@ report_rate_by_hf <- report_status_by_hf_and_date |>
   arrange(mean_report_rate)
 View(report_rate_by_hf)
 
-### Spatial distribution of missingness
+### Plot Map ----------------------------------------------------------------
 report_rate_by_hf_coords <-
   report_rate_by_hf |> inner_join(hfs) |>
   mutate(Latitude = str_replace(Latitude, ",", ".")) |>
@@ -231,3 +244,5 @@ report_rate_by_hf_coords <-
 
 tmap::tm_shape(report_rate_by_hf_coords) +
   tmap::tm_dots(col = "mean_report_rate", size = 0.01, alpha = 0.5)
+
+
