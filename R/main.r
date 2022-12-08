b83e62e9496c6ed0a31f5f74c296e9116a0ffd43
sepmein <sepmein@gmail.com>
2022-12-08 21:40:52 +0100

    added config funtion

--- R/main.r
+++ R/main.r
@@ -7,6 +7,57 @@ set_country <- function(root_folder, country) {
   snt_country <<- country
 }
 
+#' @export
+config <- function(country,
+                   root = FALSE,
+                   input_root = FALSE,
+                   output_root = FALSE,
+                   raster_root = FALSE,
+                   map_folder = FALSE,
+                   ihme_folder = FALSE,
+                   rainfall_folder = FALSE,
+                   rainfall_output = FALSE) {
+  # todo add a formatter function to format country to ISO3 code
+  country <- country
+
+  if (!(isFALSE(root))) {
+    input_root <- file.path(root, input_root)
+    output_root <- file.path(root, output_root)
+    raster_root <- file.path(root, raster_root)
+  }
+
+  # output folder
+  if (!(isFALSE(output_root))) {
+    # set by relative path
+    rainfall_output <- file.path(raster_root, rainfall_output)
+    map_output <- file.path(raster_root, map_output)
+    ihme_output <- file.path(raster_root, ihme_output)
+  }
+
+  # raster root folder
+  if (!(isFALSE(raster_root))) {
+    # Set by relative path
+    rainfall_folder <- file.path(raster_root, rainfall_folder)
+    map_folder <- file.path(raster_root, map_folder)
+    ihme_folder <- file.path(raster_root, ihme_folder)
+  }
+  result <- list(
+    "country" = country,
+    "raster" = list(
+      "rainfall" = list(
+        "folder" = rainfall_folder
+      ),
+      "map" = list(
+        "folder" = map_folder
+      ),
+      "ihme" = list(
+        "folder" = ihme_folder
+      )
+    )
+  )
+  return(result)
+}
+
 # Resource ----------------------------------------------------------------
 #' @export
 Resource <- R6::R6Class(
