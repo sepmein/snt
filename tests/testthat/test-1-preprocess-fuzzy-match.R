612c651c787c8623e68405f641451de728776ebe
sepmein <sepmein@gmail.com>
2023-01-09 22:50:01 +0100

    added preprocess fn

---
+++ tests/testthat/test-1-preprocess-fuzzy-match.R
@@ -0,0 +1,7 @@
+test_that("fuzzy match", {
+  df <- data.frame(
+    col = c("a", "b", "c", "aa", "foo", "bar", "baz", "qux", "quux", "quuz", "corge", "grault", "garply", "waldo", "fred", "plugh", "xyzzy", "thud")
+  )
+  result <- fuzzy_match(df, "col")
+  expect_equal(2 * 2, 4)
+})
