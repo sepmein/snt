5cb4d950365f1d412bf93912d31f99e6fb8cb60e
sepmein <sepmein@gmail.com>
2023-01-09 23:20:01 +0100

    fixed warnings

--- tests/testthat/test-1-preprocess-fuzzy-match.R
+++ tests/testthat/test-1-preprocess-fuzzy-match.R
@@ -1,6 +1,10 @@
 test_that("fuzzy match", {
   df <- data.frame(
-    col = c("a", "b", "c", "aa", "foo", "bar", "baz", "qux", "quux", "quuz", "corge", "grault", "garply", "waldo", "fred", "plugh", "xyzzy", "thud")
+    col <- c(
+      "a", "b", "c", "aa", "foo",
+      "bar", "baz", "qux", "quux", "quuz", "corge",
+      "grault", "garply", "waldo", "fred", "plugh", "xyzzy", "thud"
+    )
   )
   result <- fuzzy_match(df, "col")
   expect_equal(2 * 2, 4)
