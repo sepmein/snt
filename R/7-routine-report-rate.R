7724c1d87e97414861fdd1ef00de3f2c8a4e80a6
sepmein <sepmein@gmail.com>
2023-02-23 14:56:27 +0100

    added report rate calculation

--- R/7-routine-report-rate.R
+++ R/7-routine-report-rate.R
@@ -1,54 +1,60 @@
+#' Calculate the reporting rate for each indicator
+#' @param df A data frame
+#' @param ... Columns to calculate the reporting rate, exluding year and month
+#' @return Dataframe
+#' @import dplyr
+#' @importFrom rlang enquos
+#' @importFrom tibble tibble
+#' @export
 report_rate <- function(df, ...) {
-    rlang::
+    args <- enquos(...)
+    hf_list <- df |>
+        select(!!!args) |>
+        unique()
+    min_year <- min(df$year)
+    max_year <- max(df$year)
+    year <- tibble(year = min_year:max_year)
+    month <- tibble(month = 1:12)
+    ## generate a list of adm0, adm1, adm2, adm3, hfname, year, month
+    hf_list_ym <- hf_list |>
+        cross_join(year) |>
+        cross_join(month)
+    exp <- hf_list_ym |>
+        mutate(
+            exp = 1
+        )
+    ## for each adm0, adm1, adm2, adm3, hfname, year, month
+    ## calculate the reporting rate for each indicator
+    ## summing over all, if it is zero,
+    ## then consider it as not reported, else reported
+    rep <- df |>
+        mutate(
+            rep = if_else(rowSums(
+                across(
+                    !c(!!!args, year, month)
+                ),
+                na.rm = TRUE
+            ) == 0, 0, 1)
+        ) |>
+        select(
+            !!!args, year, month,
+            rep
+        )
 
+    reprate <- exp |>
+        left_join(rep) |>
+        # filling NA with 0
+        mutate(
+            rep = if_else(is.na(rep), 0, rep)
+        )
+
+    reprate |>
+        group_by(!!!args) |>
+        summarize(
+            exp = sum(exp),
+            rep = sum(rep)
+        ) |>
+        mutate(
+            reprate = rep / exp
+        )
 }
-## generate a list of adm0, adm1, adm2, adm3, hfname, year, month
-hf_list <- routine |>
-    select(adm0, adm1, adm2, adm3, hfname) |>
-    unique()
-year <- tibble(year = 2018:2022)
-month <- tibble(month = 1:12)
-hf_list_ym <- hf_list |>
-    cross_join(year) |>
-    cross_join(month)
-
-exp <- hf_list_ym |>
-    mutate(
-        exp = 1
-    )
-## for each adm0, adm1, adm2, adm3, hfname, year, month
-## calculate the reporting rate for each indicator
-## summing over all, if it is zero,
-## then consider it as not reported, else reported
-rep <- routine |>
-    mutate(
-        rep = if_else(rowSums(
-            across(
-                allout_u5:andth
-            ),
-            na.rm = TRUE
-        ) == 0, 0, 1)
-    ) |>
-    select(
-        adm0, adm1, adm2, adm3, hfname,
-        year, month,
-        rep
-    )
-
-reprate <- exp |>
-    left_join(rep) |>
-    # filling NA with 0
-    mutate(
-        rep = if_else(is.na(rep), 0, rep)
-    )
-
-reprate |>
-    group_by(adm0, adm1, adm2, adm3, hfname) |>
-    summarize(
-        exp = sum(exp),
-        rep = sum(rep)
-    ) |>
-    mutate(
-        reprate = rep / exp
-    ) |>
-    View()
