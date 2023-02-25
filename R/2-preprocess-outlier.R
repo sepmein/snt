91c5b6fcaf4cf3ab1c5c1619e6d345a0fbcdb235
Chunzhe ZHANG <sepmein@gmail.com>
2023-02-26 00:58:15 +0100

    added plot outliers fn

--- R/2-preprocess-outlier.R
+++ R/2-preprocess-outlier.R
@@ -58,3 +58,100 @@ find_outliers <-
       # 9. Return the result tibble.
     return(result)
   }
+
+#' @title Plot outliers
+#' @description Plot outliers
+#' @param df dataframe
+#' @export
+plot_outliers <- function(df) {
+  df |>
+    select(
+      year,
+      allout_ov5,
+      allout_u5,
+      allout
+    ) |>
+    pivot_longer(
+      cols = 2:4,
+      names_to = "index",
+      values_to = "value"
+    ) |>
+    mutate(year = factor(year)) |>
+    ggplot(aes(x = value, y = index)) +
+    # geom_boxplot() +
+    geom_point(aes(color = year), alpha = 0.5)
+
+  # ggsave(
+  #   "SLE_outliers_1.eps",
+  #   width = 5,
+  #   height = 4,
+  #   dpi = 300
+  # )
+
+  df |>
+    select(
+      year,
+      maldth_ov5,
+      maldth_u5,
+      maldth,
+      maltreat_ov5,
+      maltreat_u5,
+      maltreat,
+      conf,
+      test
+    ) |>
+    pivot_longer(
+      cols = 2:9,
+      names_to = "index",
+      values_to = "value"
+    ) |>
+    mutate(year = factor(year)) |>
+    ggplot(aes(x = value, y = index)) +
+    geom_point(aes(color = year), alpha = 0.5)
+  # mutate(year= factor(year)) |>
+  # ggplot(aes(x = value, y = index)) +
+  # geom_boxplot() +
+  # # geom_point(aes(shape = year),alpha = 0.01)
+  # ggstatsplot::ggbetweenstats(x = index,
+  #                             y = value,
+  #                             pairwise.comparisons = FALSE)
+  # ggsave(
+  #   "SLE_outliers_2.eps",
+  #   width = 5,
+  #   height = 4,
+  #   dpi = 300
+  # )
+
+  df |>
+    select(
+      year,
+      maladm,
+      maladm_ov5,
+      maladm_u5,
+      alladm,
+      alladm_ov5,
+      alladm_u5,
+    ) |>
+    pivot_longer(
+      cols = 2:7,
+      names_to = "index",
+      values_to = "value"
+    ) |>
+    # mutate(year= factor(year)) |>
+    # ggplot(aes(x = value, y = index)) +
+    # geom_boxplot() +
+    # geom_point(aes(shape = year),alpha = 0.01)
+    mutate(year = factor(year)) |>
+    ggplot(aes(x = value, y = index)) +
+    # geom_boxplot() +
+    geom_point(aes(color = year), alpha = 0.5)
+  # ggstatsplot::ggbetweenstats(x = index,
+  #                             y = value,
+  #                             pairwise.comparisons = FALSE)
+  # ggsave(
+  #   "SLE_outliers_3.eps",
+  #   width = 5,
+  #   height = 4,
+  #   dpi = 300
+  # )
+}
